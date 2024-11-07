01fcb8b6ebb1e1601e113767c708e8aea7a99662
indexman <ivaniohanness@gmail.com>
2024-11-07 09:29:59 +0100

    Header.js updated to have side drawer

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,42 +3,107 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-// import IconButton from "@mui/material/IconButton";
-// import MenuIcon from "@mui/icons-material/Menu";
-import { Box } from "@mui/material";
-import  { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Box, Drawer } from "@mui/material";
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { styled, useTheme } from '@mui/material/styles';
+import HomeIcon from '@mui/icons-material/Home';
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+import OnDeviceTrainingIcon from '@mui/icons-material/OnDeviceTraining';
+
+const drawerWidth = 240;
+
+const ToolStyle = styled("div")(({ theme }) => ({
+  height: theme.mixins.toolbar.height
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
 
 function Header() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-  const ToolStyle = styled("div")(({ theme }) => ({
-    style:{
-        height: theme.mixins.toolbar.height
-    }
-}))
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Box sx={{ position: "relative", zIndex: 1100 }}>
       <AppBar
-        // position="fixed"
         variant="permanent"
         sx={{ mb: "50px", backgroundColor: "secondary.main", top: 0 }}
       >
         <ToolStyle />
         <Toolbar>
-          {/* <IconButton
-            edge="start"
+          <IconButton
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
           >
             <MenuIcon />
-          </IconButton> */}
+          </IconButton>
           <Typography variant="h5" sx={{ flexGrow: 1 }}>
             IndexBlogs
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Home', 'Posts', 'Contact'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index === 0 ? <HomeIcon /> : ''}
+                  {index === 1 ? <LocalPostOfficeIcon /> : ''}
+                  {index === 2 ? <OnDeviceTrainingIcon /> : ''}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </Box>
   );
 }
